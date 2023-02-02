d24cccbc0363e5ce38e4491edaaeb1bc500b00fd
idangdev <idang.dev@gmail.com>
2023-02-02 11:12:07 +0700

    MENU MANAGEMENT (PART 1)

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,34 +12,60 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Administrator
-            </div>
+            <!-- QUERY MENU -->
+            <?php
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+            $role_id = $this->session->userdata('role_id');
+            $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                            FROM `user_menu` JOIN `user_access_menu`
+                              ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                           WHERE `user_access_menu`.`role_id` = $role_id
+                        ORDER BY `user_access_menu`.`menu_id` ASC
+            ";
+            $menu = $this->db->query($queryMenu)->result_array();
+            // var_dump($menu);
+            // die;
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+            ?>
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                User
-            </div>
+            <!-- LOOPING MENU -->
+            <?php foreach ($menu as $m) : ?>
 
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>My Profile</span></a>
-            </li>
+                <div class="sidebar-heading">
+                    <?php echo $m['menu'] ?>
+                </div>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+                <!-- SIAPKAN SUB-MENU SESUAI MENU -->
+                <?php
+                $menuId = $m['id'];
+                // $querySubMenu = " SELECT *
+                //                     FROM `user_sub_menu` JOIN `user_menu`
+                //                       ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                //                    WHERE `user_sub_menu`.`menu_id` = $menuId
+                //                      AND `user_sub_menu`.`is_active` = 1
+                // ";
+
+                $querySubMenu = "SELECT * FROM `user_sub_menu`
+                                  WHERE `menu_id` = $menuId
+                                    AND `is_active` = 1";
+
+                $subMenu = $this->db->query($querySubMenu)->result_array();
+                ?>
+
+                <?php foreach ($subMenu as $sm) : ?>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url($sm['url']) ?>">
+                            <i class="<?php echo $sm['icon']; ?>"></i>
+                            <span><?php echo $sm['title']; ?></span></a>
+                    </li>
+
+                <?php endforeach; ?>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+            <?php endforeach; ?>
 
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo base_url('auth/logout') ?>">
